7df8fc9cc394a2b7a40fa9244e4c0ff1e2e26317
vaziliybober <vaziliybober@gmail.com>
2022-03-29 19:30:15 +0300

    remove autogenerated stuff and fill something in

--- app/root.tsx
+++ app/root.tsx
@@ -41,7 +41,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full min-w-[375px]">
       <head>
         <Meta />
         <Links />
